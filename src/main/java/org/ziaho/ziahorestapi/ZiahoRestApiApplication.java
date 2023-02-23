5ee8f1bdf68911e5bc9a74fe0800bd5ee16c91fa
zziaho <rlawlgh1100@naver.com>
2023-02-23 23:09:37 +0900

    author 변경

--- src/main/java/org/ziaho/ziahorestapi/ZiahoRestApiApplication.java
+++ src/main/java/org/ziaho/ziahorestapi/ZiahoRestApiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ZiahoRestApiApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ZiahoRestApiApplication.class, args);
 	}
 
