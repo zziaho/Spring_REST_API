951e9d3603a7376a156ff5e38549b2e772de8eb3
zziaho <rlawlgh1100@naver.com>
2023-02-27 22:38:10 +0900

    Swagger API 문서 자동화 설정  - SpringBoot 3.0 이상에서 Swagger 사용 오류로 인한 주석처리

--- src/main/java/org/ziaho/ziahorestapi/config/SwaggerConfiguration.java
+++ src/main/java/org/ziaho/ziahorestapi/config/SwaggerConfiguration.java
@@ -1,33 +1,33 @@
-package org.ziaho.ziahorestapi.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-@Configuration
-@EnableSwagger2
-public class SwaggerConfiguration {
-
-    @Bean
-    public Docket swaggerApi() {
-        return new Docket(DocumentationType.SWAGGER_2).apiInfo(swaggerInfo()).select()
-                .apis(RequestHandlerSelectors.basePackage("org.ziaho.ziahorestapi.controller"))
-                // basePackage : 지정한 pacakge 경로 하단의 Controller 내용을 읽어 매핑된 resource들을 문서화 시킵니다.
-                .paths(PathSelectors.any())
-                .build()
-                .useDefaultResponseMessages(false); // 기본으로 세팅되는 200, 401, 403, 404 메세지를 표시 하지 않음
-    }
-
-    private ApiInfo swaggerInfo() {
-        return new ApiInfoBuilder().title("Spring API Documentation")
-                .description("앱 개발시 사용되는 서버 API에 대한 연동 문서입니다.")
-                .license("zzziaho").licenseUrl("https://ziaho0403.tistory.com/").version("1").build();
-    }
-
-}
+//package org.ziaho.ziahorestapi.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import springfox.documentation.builders.ApiInfoBuilder;
+//import springfox.documentation.builders.PathSelectors;
+//import springfox.documentation.builders.RequestHandlerSelectors;
+//import springfox.documentation.service.ApiInfo;
+//import springfox.documentation.spi.DocumentationType;
+//import springfox.documentation.spring.web.plugins.Docket;
+//import springfox.documentation.swagger2.annotations.EnableSwagger2;
+//
+//@Configuration
+//@EnableSwagger2
+//public class SwaggerConfiguration {
+//
+//    @Bean
+//    public Docket swaggerApi() {
+//        return new Docket(DocumentationType.SWAGGER_2).apiInfo(swaggerInfo()).select()
+//                .apis(RequestHandlerSelectors.basePackage("org.ziaho.ziahorestapi.controller"))
+//                // basePackage : 지정한 pacakge 경로 하단의 Controller 내용을 읽어 매핑된 resource들을 문서화 시킵니다.
+//                .paths(PathSelectors.any())
+//                .build()
+//                .useDefaultResponseMessages(false); // 기본으로 세팅되는 200, 401, 403, 404 메세지를 표시 하지 않음
+//    }
+//
+//    private ApiInfo swaggerInfo() {
+//        return new ApiInfoBuilder().title("Spring API Documentation")
+//                .description("앱 개발시 사용되는 서버 API에 대한 연동 문서입니다.")
+//                .license("zzziaho").licenseUrl("https://ziaho0403.tistory.com/").version("1").build();
+//    }
+//
+//}
--- src/main/java/org/ziaho/ziahorestapi/controller/v1/UserController.java
+++ src/main/java/org/ziaho/ziahorestapi/controller/v1/UserController.java
@@ -1,7 +1,5 @@
 package org.ziaho.ziahorestapi.controller.v1;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -10,7 +8,7 @@ import org.ziaho.ziahorestapi.repo.UserJpaRepo;
 
 import java.util.List;
 
-@Api(tags = {"1. User"}) // UserController를 대표하는 최상단 타이틀 영역에 표시될 값을 세팅합니다.
+//@Api(tags = {"1. User"}) // UserController를 대표하는 최상단 타이틀 영역에 표시될 값을 세팅합니다.
 @RequiredArgsConstructor // class상단에 선언하면 class내부에 final로 선언된 객체에 대해서 Constructor Injection(의존성 주입)을 수행합니다.
                         // 해당 어노테이션을 사용하지 않고 선언된 객체에 @Autowired를 사용해도 됩니다.
 @RestController // 결과 데이터를 JSON형식으로 내보냅니다.
@@ -19,7 +17,7 @@ public class UserController {
 
     private final UserJpaRepo userJpaRepo;
 
-    @ApiOperation(value = "회원 조회", notes = "모든 회원을 조회한다.") // 각각의 리소스에 제목과 설명을 표시하기 위해 세팅합니다.
+//    @ApiOperation(value = "회원 조회", notes = "모든 회원을 조회한다.") // 각각의 리소스에 제목과 설명을 표시하기 위해 세팅합니다.
     @GetMapping(value = "/user")
     public List<User> findAllUser() { // user테이블에 있는 데이터를 모두 읽어옵니다. 데이터가 한개 이상 일 수 있으므로 리턴타입은 List<User>로 선언합니다.
         // Jpa를 사용하면 기본적으로 CRUD에 대해서는 별다른 설정없이 쿼리를 질의할 수 있도록 메소드를 지원합니다.
@@ -27,7 +25,7 @@ public class UserController {
         return userJpaRepo.findAll();
     }
 
-    @ApiOperation(value = "회원 입력", notes = "모든 회원을 입력한다.") // 각각의 리소스에 제목과 설명을 표시하기 위해 세팅합니다.
+//    @ApiOperation(value = "회원 입력", notes = "모든 회원을 입력한다.") // 각각의 리소스에 제목과 설명을 표시하기 위해 세팅합니다.
     @PostMapping(value = "/user")
     public User save(@ApiParam(value = "회원아이디", required = true) @RequestParam String uid,
                      @ApiParam(value = "이름", required = true) @RequestParam String name) { // 파라미터에 대한 설명을 보여주기 위해 세팅합니다.
