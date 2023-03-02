9a2366d6297939fa309f1b6d8b2e647e62e66268
zziaho <rlawlgh1100@naver.com>
2023-03-02 21:30:18 +0900

    Swagger Api 문서 자동화 설정하기  - Swagger 문서 설정

--- src/main/java/org/ziaho/ziahorestapi/config/SwaggerConfiguration.java
+++ src/main/java/org/ziaho/ziahorestapi/config/SwaggerConfiguration.java
@@ -1,12 +1,28 @@
-//package org.ziaho.ziahorestapi.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//
-//@Configuration
-//public class SwaggerConfiguration {
-//
+package org.ziaho.ziahorestapi.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Spring API Documentation",
+                description = "앱 개발시 사용되는 서버 API에 대한 연동 문서입니다.",
+                version = "1",
+                contact = @Contact(
+                        name = "ziaho",
+                        email = "rlawlgh1100@gamil.com"
+                )
+        )
+)
+
+@Configuration
+public class SwaggerConfiguration {
+
+
+
 //    @Bean
 //    public Docket swaggerApi() {
 //        return new Docket(DocumentationType.SWAGGER_2).apiInfo(swaggerInfo()).select()
@@ -22,5 +38,5 @@
 //                .description("앱 개발시 사용되는 서버 API에 대한 연동 문서입니다.")
 //                .license("zzziaho").licenseUrl("https://ziaho0403.tistory.com/").version("1").build();
 //    }
-//
-//}
+
+}
