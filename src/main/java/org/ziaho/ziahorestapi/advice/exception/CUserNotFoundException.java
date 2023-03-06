434fcdb5f2f37f08a0b5b0c2bb8e517962425bee
zziaho <rlawlgh1100@naver.com>
2023-03-06 20:16:46 +0900

    ControllerAdvice를 이용한 Exception 처리  - 커스텀 Exception 정의(CUserNotFoundException)

---
+++ src/main/java/org/ziaho/ziahorestapi/advice/exception/CUserNotFoundException.java
@@ -0,0 +1,17 @@
+package org.ziaho.ziahorestapi.advice.exception;
+
+public class CUserNotFoundException extends RuntimeException {
+
+    public CUserNotFoundException(String msg, Throwable t) {
+        super(msg, t);
+    }
+
+    public  CUserNotFoundException(String msg) {
+        super(msg);
+    }
+
+    public CUserNotFoundException() {
+        super();
+    }
+
+}
