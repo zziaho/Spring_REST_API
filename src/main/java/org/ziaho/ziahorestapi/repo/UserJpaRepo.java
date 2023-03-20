518a234a41a19bc45821167ad6258b0d5c03d63a
zziaho <rlawlgh1100@naver.com>
2023-03-20 22:41:01 +0900

    SpringSecurity를 이용한 인증 및 권한부여  - UserJpaRepo에 findByUid 추가

--- src/main/java/org/ziaho/ziahorestapi/repo/UserJpaRepo.java
+++ src/main/java/org/ziaho/ziahorestapi/repo/UserJpaRepo.java
@@ -3,6 +3,10 @@ package org.ziaho.ziahorestapi.repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.ziaho.ziahorestapi.entity.User;
 
+import java.util.Optional;
+
 public interface UserJpaRepo extends JpaRepository<User, Long> {
+
+    Optional<User> findById(String email);
 
 }
