82e1cdedfe7066926e8c1d1bd606c3b00aa4e79c
zziaho <rlawlgh1100@naver.com>
2023-03-20 22:40:50 +0900

    SpringSecurity를 이용한 인증 및 권한부여  - User Entity 수정

--- src/main/java/org/ziaho/ziahorestapi/entity/User.java
+++ src/main/java/org/ziaho/ziahorestapi/entity/User.java
@@ -1,10 +1,19 @@
 package org.ziaho.ziahorestapi.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Builder // builder를 사용할 수 있게 됩니다.
 @Entity // jpa entity임을 알립니다.
@@ -12,7 +21,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor // 인자가 없는 생성자를 자동으로 생성합니다.
 @AllArgsConstructor // 인자를 모두 갖춘 생성자를 자동으로 생성합니다.
 @Table(name = "user") // 'user' 테이블과 매핑됨을 알립니다.
-public class User {
+public class User implements UserDetails {
 
     @Id // primaryKey임을 명시합니다.
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -27,5 +36,47 @@ public class User {
     @Column(nullable = false, length = 100)
     // name column을 명시,  null값이 될 수 없으며(필수값) 최대 길이는 100
     private String name;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Column(nullable = false, length = 100)
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Builder.Default
+    private List<String> roles = new ArrayList<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUsername() {
+        return this.uid;
+    }
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 
 }
