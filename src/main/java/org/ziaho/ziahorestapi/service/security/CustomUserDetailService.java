3fa4b8546294edc6c819e85df2e7394ce07f1a20
zziaho <rlawlgh1100@naver.com>
2023-03-20 22:40:43 +0900

    SpringSecurity를 이용한 인증 및 권한부여  - Custom UserDetailService 정의

---
+++ src/main/java/org/ziaho/ziahorestapi/service/security/CustomUserDetailService.java
@@ -0,0 +1,20 @@
+package org.ziaho.ziahorestapi.service.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+import org.ziaho.ziahorestapi.advice.exception.CUserNotFoundException;
+import org.ziaho.ziahorestapi.repo.UserJpaRepo;
+
+@RequiredArgsConstructor
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final UserJpaRepo userJpaRepo;
+
+    public UserDetails loadUserByUsername(String userPk) {
+        return userJpaRepo.findById(Long.valueOf(userPk)).orElseThrow(CUserNotFoundException::new);
+    }
+
+}
