4a1082a0b86c7aa4b73bcfdb9d21106ee5f7471c
zziaho <rlawlgh1100@naver.com>
2023-03-26 19:35:27 +0900

    SpringSecurity를 이용한 인증 및 권한부여(예외처리 보완)  - CustomAccessDeniedHandler 생성

---
+++ src/main/java/org/ziaho/ziahorestapi/config/security/CustomAccessDeniedHandler.java
@@ -0,0 +1,22 @@
+package org.ziaho.ziahorestapi.config.security;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        response.sendRedirect("/exception/accessdenied");
+    }
+
+}
