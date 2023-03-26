3318b4111cd59e42b300348f52abf927e7148c64
zziaho <rlawlgh1100@naver.com>
2023-03-26 19:32:52 +0900

    SpringSecurity를 이용한 인증 및 권한부여  - UserController에 X-AUTH-TOKEN을 인자로 받도록 선언

--- src/main/java/org/ziaho/ziahorestapi/controller/v1/UserController.java
+++ src/main/java/org/ziaho/ziahorestapi/controller/v1/UserController.java
@@ -4,6 +4,7 @@ package org.ziaho.ziahorestapi.controller.v1;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -15,7 +16,7 @@ import org.ziaho.ziahorestapi.model.response.SingleResult;
 import org.ziaho.ziahorestapi.repo.UserJpaRepo;
 import org.ziaho.ziahorestapi.service.ResponseService;
 
-@Tag(name = "1. User", description = "UserController") // UserController를 대표하는 최상단 타이틀 영역에 표시될 값을 세팅합니다.
+@Tag(name = "2. User", description = "UserController") // UserController를 대표하는 최상단 타이틀 영역에 표시될 값을 세팅합니다.
 @RequiredArgsConstructor // class상단에 선언하면 class내부에 final로 선언된 객체에 대해서 Constructor Injection(의존성 주입)을 수행합니다.
                         // 해당 어노테이션을 사용하지 않고 선언된 객체에 @Autowired를 사용해도 됩니다.
 @RestController // 결과 데이터를 JSON형식으로 내보냅니다.
@@ -25,6 +26,9 @@ public class UserController {
     private final UserJpaRepo userJpaRepo;
     private final ResponseService responseService; // 결과를 처리할 Service
 
+    @Parameters({
+            @Parameter(name = "X-AUTH-TOKEN", description = "로그인 성공 후 access_token", required = true)
+    })
     @Operation(summary = "회원 리스트 조회", description = "모든 회원을 조회한다.") // 각각의 리소스에 제목과 설명을 표시하기 위해 세팅합니다.
     @GetMapping(value = "/users")
     public ListResult<User> findAllUser() { // user테이블에 있는 데이터를 모두 읽어옵니다. 데이터가 한개 이상 일 수 있으므로 리턴타입은 List<User>로 선언합니다.
@@ -35,6 +39,9 @@ public class UserController {
         return responseService.getListResult(userJpaRepo.findAll());
     }
 
+    @Parameters({
+            @Parameter(name = "X-AUTH-TOKEN", description = "로그인 성공 후 access_token", required = true)
+    })
     @Operation(summary = "회원 단건 조회", description = "msrl로 회원을 조회한다.")
     @GetMapping(value = "/user/{msrl}")
     public SingleResult<User> findUserById(@Parameter(name = "회원번호", required = true) @PathVariable long msrl,
@@ -43,6 +50,9 @@ public class UserController {
         return responseService.getSingleResult(userJpaRepo.findById(msrl).orElseThrow(CUserNotFoundException::new));
     }
 
+    @Parameters({
+            @Parameter(name = "X-AUTH-TOKEN", description = "로그인 성공 후 access_token", required = true)
+    })
     @Operation(summary = "회원 입력", description = "회원을 입력한다.") // 각각의 리소스에 제목과 설명을 표시하기 위해 세팅합니다.
     @PostMapping(value = "/user")
     public SingleResult<User> save(@Parameter(name = "회원아이디", required = true) @RequestParam(value = "uid") String uid,
@@ -58,6 +68,9 @@ public class UserController {
         return responseService.getSingleResult(userJpaRepo.save(user));
     }
 
+    @Parameters({
+            @Parameter(name = "X-AUTH-TOKEN", description = "로그인 성공 후 access_token", required = true)
+    })
     @Operation(summary = "회원 수정", description = "회원정보를 수정한다.")
     @PutMapping(value = "/user")
     public SingleResult<User> modify(
@@ -73,6 +86,9 @@ public class UserController {
         return responseService.getSingleResult(userJpaRepo.save(user));
     }
 
+    @Parameters({
+            @Parameter(name = "X-AUTH-TOKEN", description = "로그인 성공 후 access_token", required = true)
+    })
     @Operation(summary = "회원 삭제", description = "msrl로 회원정보를 삭제한다")
     @DeleteMapping(value = "/user/{msrl}")
     public CommonResult delete(
