6c109a897165e1ca4b658501c21df1f69ac82788
zziaho <rlawlgh1100@naver.com>
2023-03-26 19:34:38 +0900

    SpringSecurity를 이용한 인증 및 권한부여(예외처리 보완)  - ExceptionAdvice 내용 추가

--- src/main/java/org/ziaho/ziahorestapi/advice/ExceptionAdvice.java
+++ src/main/java/org/ziaho/ziahorestapi/advice/ExceptionAdvice.java
@@ -5,9 +5,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.ziaho.ziahorestapi.advice.exception.CAuthenticationEntryPointException;
 import org.ziaho.ziahorestapi.advice.exception.CEmailSigninFailedException;
 import org.ziaho.ziahorestapi.advice.exception.CUserNotFoundException;
 import org.ziaho.ziahorestapi.model.response.CommonResult;
@@ -31,14 +33,14 @@ public class ExceptionAdvice {
 //        return responseService.getFailResult();
 //    }
 
-//    @ExceptionHandler(Exception.class)
-//    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-//    protected CommonResult defaultException(HttpServletRequest request,
-//                                            Exception e) {
-//        return responseService.getFailResult(Integer.parseInt(getMessage("unKnown.code")),
-//                getMessage("unKnown.message")
-//        );
-//    }
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    protected CommonResult defaultException(HttpServletRequest request,
+                                            Exception e) {
+        return responseService.getFailResult(Integer.parseInt(getMessage("unKnown.code")),
+                getMessage("unKnown.message")
+        );
+    }
 
     @ExceptionHandler(CUserNotFoundException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
@@ -55,6 +57,18 @@ public class ExceptionAdvice {
         return responseService.getFailResult(
                 Integer.valueOf(getMessage("emailSigninFailed.code")),
                 getMessage("emailSigninFailed.message"));
+    }
+
+    @ExceptionHandler(CAuthenticationEntryPointException.class)
+    public CommonResult authenticationEntryPointException(HttpServletRequest request, CAuthenticationEntryPointException e) {
+        return responseService.getFailResult(Integer.valueOf(getMessage("entryPointException.code")),
+                getMessage("entryPointException.message"));
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public CommonResult AccessDeniedException(HttpServletRequest request, AccessDeniedException e) {
+        return responseService.getFailResult(Integer.valueOf(getMessage("accessDenied.code")),
+                getMessage("accessDenied.message"));
     }
 
     // code 정보에 해당하는 메시지를 조회합니다.
