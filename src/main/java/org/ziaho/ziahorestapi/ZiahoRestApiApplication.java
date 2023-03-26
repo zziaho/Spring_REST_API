3cd8475d36e3008ade35cd7703059c911d67e9d6
zziaho <rlawlgh1100@naver.com>
2023-03-26 19:32:10 +0900

    SpringSecurity를 이용한 인증 및 권한부여  - passwordEncoder Bean 추가

--- src/main/java/org/ziaho/ziahorestapi/ZiahoRestApiApplication.java
+++ src/main/java/org/ziaho/ziahorestapi/ZiahoRestApiApplication.java
@@ -2,6 +2,9 @@ package org.ziaho.ziahorestapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class ZiahoRestApiApplication {
@@ -9,6 +12,11 @@ public class ZiahoRestApiApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(ZiahoRestApiApplication.class, args);
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
 	}
 
 }
